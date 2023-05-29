ed90ad5a3c452f3661bf7f29fc82f1532b1cce26
Mohammad Nazir Sharifi <nazirsharifi19@gmail.com>
2023-05-29 22:33:00 +0300

    window sidebar completed!

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # needed libraries
 from tkinter import *
 from PIL import Image, ImageTk
-from config import *
-import utils
+from utils import *
 
 
 # window setup
@@ -11,10 +10,11 @@ window.title('Minesweeper Game')
 window.config(bg=WINDOW_BG)
 window.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
 window.resizable(False, False)
+window.config(cursor="hand2")
 
 
 # window header setup
-header = Frame(window, bg=WINDOW_BG, width=WINDOW_WIDTH, height=utils.calculate_percentage('h', 15))
+header = Frame(window, bg=WINDOW_BG, width=WINDOW_WIDTH, height=calculate_percentage('h', 15))
 header.place(x=0, y=0)
 # app logo
 logo = Image.open("logo.png")
@@ -26,7 +26,32 @@ header_title.config(image=logo, compound='left')
 header_title.place(relx=0.5, rely=0.5, anchor='center')
 
 
+# window main setup
+def start_game():
+    startButton.config(state=DISABLED)
+    endButton.config(state=NORMAL)
+    # this part will be updated...
+    print('Game started!')
+
+
+def end_game():
+    startButton.config(state=NORMAL)
+    endButton.config(state=DISABLED)
+    # this part will be updated...
+    print('Game end!')
+
+
 # window sidebar setup
+sidebar = Frame(window, bg=WINDOW_BG, width=calculate_percentage('w', 25), height=calculate_percentage('h', 80))
+sidebar.place(x=0, y=calculate_percentage('h', 20))
+# start game btn
+startButton = Button(sidebar, text='Start Game', bg='#539165', fg='#fff', width=17, height=1)
+startButton.config(font=('Arial', 12, 'bold'), command=start_game)
+startButton.grid(row=0, column=0, pady=5, padx=5)
+# end game btn
+endButton = Button(sidebar, text='End Game', bg='#F96666', fg='#fff', width=17, height=1)
+endButton.config(font=('Arial', 12, 'bold'), command=end_game, state=DISABLED)
+endButton.grid(row=1, column=0, pady=5, padx=5)
 
 
 # project running...
--- utils.py
+++ utils.py
@@ -6,3 +6,4 @@ import tkinter
 # d_name: dimension name-> w=>width, h=>height
 def calculate_percentage(d_name, percentage):
     return (WINDOW_WIDTH / 100) * percentage if d_name == 'w' else (WINDOW_HEIGHT / 100) * percentage
+
